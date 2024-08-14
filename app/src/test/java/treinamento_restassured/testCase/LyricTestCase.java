209f5c6d05cc5e26d0ccb30c6379010ba8c091c1
leo_da_cruz <leonardosilveiracrux@gmail.com>
2024-08-14 01:51:20 -0300

    Criacao do TestCase para a API de Lyrics

--- app/src/test/java/treinamento_restassured/testCase/LyricTestCase.java
+++ app/src/test/java/treinamento_restassured/testCase/LyricTestCase.java
@@ -1,5 +1,38 @@
 package treinamento_restassured.testCase;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import io.qameta.allure.Description;
+import io.restassured.response.Response;
+import treinamento_restassured.service.LyricService;
+import treinamento_restassured.validation.LyricValidation;
+
 public class LyricTestCase {
-    
+private Map<String, Object> pathParametersMap;
+    private LyricService lyricService;
+    private LyricValidation lyricValidation;
+
+    public LyricTestCase() {
+        pathParametersMap = new HashMap<>();
+        lyricService = new LyricService();
+        lyricValidation =  new LyricValidation();
+    }
+
+    @Test
+    @DisplayName("Teste leo")
+    @Description("Teste leo Descricao")
+    public void testLeo() {
+        pathParametersMap.put("ARTIST", "Coldplay");
+        pathParametersMap.put("TITLE", "Adventure of a Lifetime");
+
+        Response resposta = lyricService.getLyric(pathParametersMap);
+        lyricValidation.setResponse(resposta);
+        lyricValidation.validarStatusCode(200);
+
+    }
+
 }
