4d9189345fd576a00d10746b467d3cdfcce35db4
leo_da_cruz <leonardosilveiracrux@gmail.com>
2024-08-14 01:51:09 -0300

    Criacao do Service para a API de Lyrics

--- app/src/test/java/treinamento_restassured/service/LyricService.java
+++ app/src/test/java/treinamento_restassured/service/LyricService.java
@@ -1,5 +1,40 @@
 package treinamento_restassured.service;
 
+import java.util.Map;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 public class LyricService {
-    
+
+    String baseUriProducao = "https://api.lyrics.ovh";
+    String baseUriMock = "";
+    String basePath = "/v1/{ARTIST}/{TITLE}";
+    RequestSpecBuilder requestSpecBuilder;
+
+    public LyricService() {
+
+        requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder
+                .setBaseUri(baseUriProducao)
+                .setBasePath(basePath)
+                .setContentType(ContentType.JSON)
+                .setRelaxedHTTPSValidation();
+    }
+
+    public Response getLyric(Map<String, Object> pathParametersMap) {
+
+        Response resposta = RestAssured
+                .given()
+                .spec(requestSpecBuilder.build())
+                .pathParams(pathParametersMap)
+                .filter(new AllureRestAssured())
+                .log().all()
+                .get();
+        return resposta;
+
+    }
+
 }
