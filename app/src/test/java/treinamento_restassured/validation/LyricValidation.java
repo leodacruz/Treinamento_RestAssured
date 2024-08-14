e6997277b81c8263df8f5f41f0cd41a23dff5436
leo_da_cruz <leonardosilveiracrux@gmail.com>
2024-08-14 01:51:46 -0300

    Criacao do classe de validacao para a API de Lyrics

--- app/src/test/java/treinamento_restassured/validation/LyricValidation.java
+++ app/src/test/java/treinamento_restassured/validation/LyricValidation.java
@@ -1,5 +1,40 @@
 package treinamento_restassured.validation;
 
+import java.io.File;
+
+import io.restassured.response.Response;
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
+
+import io.restassured.module.jsv.JsonSchemaValidator;
+
 public class LyricValidation {
+
+    private Response resposta;
+
+    public LyricValidation() {
+    }
+
+    public void setResponse(Response resposta) {
+        this.resposta = resposta;
+        resposta.then().log().all();
+    }
+
+    public void validarStatusCode(int statusCode){
+        resposta.then().assertThat().statusCode(statusCode);
+    }
+
+    public void validarContrato(File jsonContrato) {
+        resposta.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonContrato));
+    }
+
     
+
+
+
+
+
+
+
 }
